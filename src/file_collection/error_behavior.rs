efd4b89358f9ac02bd382f34fbd717006b732f8e
Christopher Gundler <c.gundler@mail.de>
2016-08-05 16:29:07 +0200

    Prepare documentation

--- src/file_collection/error_behavior.rs
+++
@@ -1,8 +0,0 @@
-/// This enum describes the behavior of a sorting algorithm in case of errors while adding files.
-#[derive(Debug,Clone,Copy)]
-pub enum ErrorBehavior {
-    /// Ignore the error and continue adding.
-    Ignore,
-    /// Abort the execution and stop adding files.
-    Abort
-}
--- src/order.rs
+++
@@ -1,8 +0,0 @@
-/// The order of the sorting algorithm.
-#[derive(Clone,Copy)]
-pub enum Order {
-    /// The order should be sorted in ascending order.
-    Ascending,
-    /// The order should be sorted in descending order.
-    Descending
-}
